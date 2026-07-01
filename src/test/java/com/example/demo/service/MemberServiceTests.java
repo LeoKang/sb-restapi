037c6c1e83ea731828f21885aec1bd30c5aaf6b8
seungjin <xeungjin@nate.com>
2026-07-01 10:36:06 +0900

    ~294 page

--- src/test/java/com/example/demo/service/MemberServiceTests.java
+++ src/test/java/com/example/demo/service/MemberServiceTests.java
@@ -40,4 +40,38 @@ public class MemberServiceTests {
         List<MemberResponse> results = memberService.findAll();
         assertThat(results.size()).isEqualTo(2);
     }
+
+    @Test
+    @DisplayName("트랜잭션 커밋 테스트")
+    public void testTransactionalCommit() {
+        List<MemberRequest> userRequests = List.of(
+                MemberRequest.builder().name("윤서준").email("SeojunYoon@hanbit.co.kr").age(10).build(),
+                MemberRequest.builder().name("윤광철").email("KwangcheolYoon@hanbit.co.kr").age(43).build(),
+                MemberRequest.builder().name("김도윤").email("DoyunKim@hanbit.co.kr").age(11).build(),
+                MemberRequest.builder().name("공미영").email("MiyeongKong@hanbit.co.kr").age(28).build()
+        );
+
+        try {
+            memberService.createBatch(userRequests);
+        } catch (Exception ignored) {
+        }
+        assertThat(memberRepository.count()).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("트랜잭션 롤백 테스트")
+    public void testTransactionalRollback() {
+        List<MemberRequest> userRequests = List.of(
+                MemberRequest.builder().name("윤서준").email("SeojunYoon@hanbit.co.kr").age(10).build(),
+                MemberRequest.builder().name("윤광철").email("KwangcheolYoon@hanbit.co.kr").age(43).build(),
+                MemberRequest.builder().name("김도윤").email("SeojunYoon@hanbit.co.kr").age(11).build(),
+                MemberRequest.builder().name("공미영").email("MiyeongKong@hanbit.co.kr").age(28).build()
+        );
+        try {
+            memberService.createBatch(userRequests);
+        } catch (Exception e) {
+
+        }
+        assertThat(memberRepository.count()).isEqualTo(0);
+    }
 }
